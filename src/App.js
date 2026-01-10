c458d1cec89776e46630aa7ed82dcb6c053e6e40
sivaneshkumarengg63-svg <sivaneshkumarengg63@gmail.com>
2026-01-10 19:15:57 +0000

    Add files via upload
    final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🚀 React App Running in Docker</h1>
+      <p>Hosted via AWS ECR</p>
+    </div>
+  );
+}
+
+export default App;
